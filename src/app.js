d979ffc9a33ed7924bb7fd0ca4d968aacc87b8eb
sky7th <xoghk0321@gmail.com>
2019-09-23 23:53:26 +0900

    #서버 : 기능별 리팩토링

--- src/app.js
+++ src/app.js
@@ -1,29 +1,22 @@
-const express = require('express'),
-    path = require('path'),
-    morgan = require('morgan'),
-    mysql = require('mysql'),
-    myConnection = require('express-myconnection');
-    cron = require('node-cron');
-
-const lightKeySql = require('./query/lightKeySql');
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 
 const app = express();
 
 // importing routes
 const lightKeyRoutes = require('./routes/lightKey');
 
+const randomKey = require('./utils/RandomKey');
+
+const mysqlConnection = require('./utils/DBConnect');
+
 // settings
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-var mysqlConnection = {
-    host: 'localhost',
-    user: 'root',
-    password: 'dlxoghk',
-    port: 3306,
-    database: 'light_key'
-};
 
 // middlewares
 app.use(morgan('dev'));
@@ -43,47 +36,4 @@ app.listen(app.get('port'), () => {
     console.log(`server on port ${app.get('port')}`);
 });
 
-
-
-function randomString() {
-    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    var string_length = 6;
-    var randomstring = '';
-    for (var i = 0; i < string_length; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum, rnum + 1);
-    }
-    //document.randform.randomfield.value = randomstring;
-    return randomstring;
-}
-
-var modelIdList = [];
-var connection = mysql.createConnection(mysqlConnection);
-
-var getModelIdList = () => {
-    connection.query(lightKeySql.SELECT_MODEL_ID, function (err, res, fields) {
-        if (err) {
-            console.log(err);
-        }
-        res.forEach(element => {
-            modelIdList.push(element['id']);
-        });
-    });
-}
-
-var changeModelKeyValue = (modelId, newModelKeyValue) => {
-    connection.query(lightKeySql.UPDATE_MODEL_KEY_VALUE, [newModelKeyValue, modelId], function (err, res, fields) {
-        if (err) {
-            console.log(err);
-        }
-    });
-}
-
-
-cron.schedule('1 * * * * *', () => {
-    getModelIdList();
-    modelIdList.forEach(element => {
-        changeModelKeyValue(element, randomString())
-    });
-    console.log('keys chanege !!!');
-});
+randomKey.start()
